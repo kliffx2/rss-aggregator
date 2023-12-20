e897090417f8245f7187123ea01b209749840332
Dat Nguyen <datdamin.omg21@gmail.com>
2023-12-20 18:03:54 +0700

    authen with api keys

--- handler_user.go
+++ handler_user.go
@@ -34,5 +34,9 @@ func (apiCfg *apiConfig)handlerCreateUser(w http.ResponseWriter, r *http.Request
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
 		return
 	}
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig)handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User){
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
