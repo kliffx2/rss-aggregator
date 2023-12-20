5d388198c2f65deee7c9d47543d30ccf41766e35
Dat Nguyen <datdamin.omg21@gmail.com>
2023-12-20 11:23:58 +0700

    database

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
@@ -8,7 +9,14 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	"github.com/kliffx2/rssagg/internal/database"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct{
+	DB *database.Queries
+}
 
 func main() {
 
@@ -19,6 +27,19 @@ func main() {
 		log.Fatal("PORT is not found in the enviroment")
 	}
 
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == ""{
+		log.Fatal("DB_URL is not found in the enviroment")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database: ", err);
+	}
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
@@ -33,6 +54,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReady)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -42,7 +64,7 @@ func main() {
 	}
 
 	log.Printf("Server starting on port %v", portString)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
